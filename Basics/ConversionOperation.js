3a9644b08b4097c622ca0a40e50788f0dcda96aa
Sudhanshu Kumar <sk3360240@gmail.com>
2024-10-09 16:42:14 +0000

    conversions

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,19 @@
+// let num = 33
+let num = "33abs"
+// console.log(num)
+// console.log(typeof num);
+
+let valueInNum = Number(num);
+// console.log(valueInNum);
+// console.log(typeof valueInNum)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = 0; ==> false
+// let isLoggedIn ="sudha"; ==> true
+// let isLoggedIn = "";  ==> false
+let valueInisLoggedIn = Boolean(isLoggedIn)
+console.log(valueInisLoggedIn)
+console.log(typeof(valueInisLoggedIn))
