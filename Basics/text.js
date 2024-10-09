87b6f0b84bece4a43e3c2b70350f3695ad94c2d7
Sudhanshu Kumar <sk3360240@gmail.com>
2024-10-09 16:13:32 +0000

    dataTypes

--- Basics/text.js
+++ Basics/text.js
@@ -1,1 +1,13 @@
 console.log("sudha");
+const accountId = 12434
+let accountName = "sudhanshu"
+let state;
+console.log(accountId,accountName);
+console.table([accountId,accountName])
+console.log(typeof "sudhanshu")
+console.log(typeof accountId)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof state)
+console.log(typeof Symbol)
+console.log(typeof Object)
