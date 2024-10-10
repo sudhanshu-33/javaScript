093d51a3528f226db8af50ccf5397ef4a49de290
Sudhanshu Kumar <sk3360240@gmail.com>
2024-10-10 09:38:58 +0000

    operations

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -11,9 +11,31 @@ let valueInNum = Number(num);
 // "33abc" => NaN
 //  true => 1; false => 0
 
-// let isLoggedIn = 0; ==> false
+let isLoggedIn = 0; //==> false
 // let isLoggedIn ="sudha"; ==> true
 // let isLoggedIn = "";  ==> false
 let valueInisLoggedIn = Boolean(isLoggedIn)
-console.log(valueInisLoggedIn)
-console.log(typeof(valueInisLoggedIn))
+// console.log(valueInisLoggedIn)
+// console.log(typeof(valueInisLoggedIn))
+
+let somenumber = 44;
+let StringNumber = String(somenumber);
+console.log(somenumber);
+console.log(typeof somenumber)
+
+
+
+//operations
+
+console.log("1" + 2) //==>12
+console.log(1 + "2") //=>12
+console.log(1 + "2" +1) //==>121
+console.log(1 + 2 + "2") //==>32
+console.log("1" + 2 +2) // ==> 122
+
+let x=2
+let y=x++;
+console.log(`${x},${y}`); //==>x=3,y=2
+let a=4
+let b=++a;
+console.log(`${a},${b}`)  //==> a=5,b=5
